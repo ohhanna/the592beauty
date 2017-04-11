a6a2ca5df197f61459833839017b9943427079b6
ohhanna <hue9404@hanmail.net>
2017-04-11 22:42:12 +0900

    survey_q10
    앨범/카메라에서 사진 불러오기

--- the592beauty3/app/src/main/java/com/example/hanna/the592beauty3/PersonalColorQ10.java
+++ the592beauty3/app/src/main/java/com/example/hanna/the592beauty3/PersonalColorQ10.java
@@ -3,23 +3,34 @@ package com.example.hanna.the592beauty3;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
+
+import static com.example.hanna.the592beauty3.R.id.button_upload;
 
 public class PersonalColorQ10 extends Activity {
 
     final int DIALOG_LIST = 2;
 
+    private static final int PICK_FROM_CAMERA = 1;
+    private static final int PICK_FROM_GALLERY = 2;
+    private ImageView imgview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_q10);
+
+        imgview = (ImageView) findViewById(R.id.imageView1);
 
         Button button_upload = (Button) findViewById(R.id.button_upload);
 
@@ -48,14 +59,42 @@ public class PersonalColorQ10 extends Activity {
                             public void onClick(DialogInterface dialog, int which){
                                 if(which == 0){
                                     Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                                    startActivity(intent);
+                                    intent.putExtra(MediaStore.EXTRA_OUTPUT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI.toString());
+
+                                    intent.putExtra("crop", "true");
+//                                    intent.putExtra("aspectX", 0);
+//                                    intent.putExtra("aspectY", 0);
+//                                    intent.putExtra("outputX", 200);
+//                                    intent.putExtra("outputY", 150);
+
+                                    try {
+                                        intent.putExtra("return-data", true);
+                                        startActivityForResult(intent, PICK_FROM_CAMERA);
+                                    } catch (ActivityNotFoundException e) {
+                                        // Do nothing for now
+                                    }
+
                                     dismissDialog(DIALOG_LIST);
                                 }
                                 else if(which == 1){
-                                    Intent intent2 = new Intent(Intent.ACTION_PICK);
-                                    intent2.setType(MediaStore.Images.Media.CONTENT_TYPE);
-                                    intent2.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                                    startActivity(intent2);
+                                    Intent intent = new Intent();
+                                    // Gallery 호출
+                                    intent.setType("image/*");
+                                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                                    // 잘라내기 셋팅
+                                    intent.putExtra("crop", "true");
+//                                    intent.putExtra("aspectX", 0);
+//                                    intent.putExtra("aspectY", 0);
+//                                    intent.putExtra("outputX", 200);
+//                                    intent.putExtra("outputY", 150);
+                                    try {
+                                        intent.putExtra("return-data", true);
+                                        startActivityForResult(Intent.createChooser(intent,
+                                                "Complete action using"), PICK_FROM_GALLERY);
+                                    } catch (ActivityNotFoundException e) {
+                                        // Do nothing for now
+                                    }
+
                                     dismissDialog(DIALOG_LIST);
                                 }
                             }
@@ -64,4 +103,23 @@ public class PersonalColorQ10 extends Activity {
         }
         return super.onCreateDialog(id);
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == PICK_FROM_CAMERA) {
+            Bundle extras = data.getExtras();
+            if (extras != null) {
+                Bitmap photo = extras.getParcelable("data");
+                imgview.setImageBitmap(photo);
+            }
+        }
+        if (requestCode == PICK_FROM_GALLERY) {
+            Bundle extras2 = data.getExtras();
+            if (extras2 != null) {
+                Bitmap photo = extras2.getParcelable("data");
+                imgview.setImageBitmap(photo);
+            }
+        }
+    }
+
 }
