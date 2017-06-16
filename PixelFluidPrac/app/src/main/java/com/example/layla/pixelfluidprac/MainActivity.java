aa1a286b91aec2ec7fca1cb2ef940344feedd036
heunguksangmyeong <phreshout999@gmail.com>
2017-06-17 00:01:03 +0900

    Pixel Fluid practice2

--- PixelFluidPrac/app/src/main/java/com/example/layla/pixelfluidprac/MainActivity.java
+++ PixelFluidPrac/app/src/main/java/com/example/layla/pixelfluidprac/MainActivity.java
@@ -1,115 +1,119 @@
 package com.example.layla.pixelfluidprac;
-
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-
-import java.lang.Math;
-
 import android.content.Context;
-import android.graphics.*;
-import android.view.*;
-import android.widget.ImageView;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.view.MotionEvent;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-    final int WIDTH = 50;
-    final int HEIGHT = 50;
-    final int COUNT = (WIDTH + 1) * (HEIGHT + 1);
-
-    final float[] mVerts = new float[COUNT * 2];
-    final float[] mOrig = new float[COUNT * 2];
-
-    final Matrix mMatrix = new Matrix();
-    final Matrix mInverse = new Matrix();
-    Bitmap mBitmap;
-    ImageView imgView;
-    Canvas canvas;
+//with small sample image
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        imgView = (ImageView) findViewById(R.id.imageView);
+        setContentView(new SampleView(this));
+    }
 
-        BitmapFactory.Options bfo = new BitmapFactory.Options();
-        bfo.inSampleSize = 5;
-        mBitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.dd, bfo);
+    private static class SampleView extends View {
+        private static final int WIDTH = 20;
+        private static final int HEIGHT = 20;
+        private static final int COUNT = (WIDTH + 1) * (HEIGHT + 1);
 
-        canvas.setBitmap(mBitmap);
-        imgView.setFocusable(true);
+        private final Bitmap mBitmap;
+        private final float[] mVerts = new float[COUNT * 2];
+        private final float[] mOrig = new float[COUNT * 2];
 
-        float w = mBitmap.getWidth();
-        float h = mBitmap.getHeight();
-        // construct our mesh
-        int index = 0;
-        for (int y = 0; y <= HEIGHT; y++) {
-            float fy = h * y / HEIGHT;
-            for (int x = 0; x <= WIDTH; x++) {
-                float fx = w * x / WIDTH;
-                setXY(mVerts, index, fx, fy);
-                setXY(mOrig, index, fx, fy);
-                index += 1;
+        private final Matrix mMatrix = new Matrix();
+        private final Matrix mInverse = new Matrix();
+
+        private static void setXY(float[] array, int index, float x, float y) {
+            array[index * 2 + 0] = x;
+            array[index * 2 + 1] = y;
+        }
+
+        public SampleView(Context context) {
+            super(context);
+            setFocusable(true);
+
+            mBitmap = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.dd);
+
+            float w = mBitmap.getWidth();
+            float h = mBitmap.getHeight();
+            // construct our mesh
+            int index = 0;
+            for (int y = 0; y <= HEIGHT; y++) {
+                float fy = h * y / HEIGHT;
+                for (int x = 0; x <= WIDTH; x++) {
+                    float fx = w * x / WIDTH;
+                    setXY(mVerts, index, fx, fy);
+                    setXY(mOrig, index, fx, fy);
+                    index += 1;
+                }
             }
+
+            mMatrix.setTranslate(10, 10);
+            mMatrix.invert(mInverse);
         }
-        mMatrix.setTranslate(10, 10);
-        mMatrix.invert(mInverse);
 
-        canvas.drawColor(0xFFCCCCCC);
-        canvas.concat(mMatrix);
-        canvas.drawBitmapMesh(mBitmap, WIDTH, HEIGHT, mVerts, 0, null, 0, null);
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawColor(0xFFCCCCCC);
 
-        imgView.draw(canvas);
+            canvas.concat(mMatrix);
+            canvas.drawBitmapMesh(mBitmap, WIDTH, HEIGHT, mVerts, 0, null, 0,
+                    null);
+        }
 
-    }
+        private void warp(float cx, float cy) {
+            final float K = 10000;
+            float[] src = mOrig;
+            float[] dst = mVerts;
+            for (int i = 0; i < COUNT * 2; i += 2) {
+                float x = src[i + 0];
+                float y = src[i + 1];
+                float dx = cx - x;
+                float dy = cy - y;
+                float dd = dx * dx + dy * dy;
+                float d = (float)Math.sqrt(dd);
+                float pull = K / (dd + 0.000001f);
 
+                pull /= (d + 0.000001f);
+                // android.util.Log.d("skia", "index " + i + " dist=" + d +
+                // " pull=" + pull);
 
-    private void warp(float cx, float cy) {
-        final float K = 10000;
-        float[] src = mOrig;
-        float[] dst = mVerts;
-        for (int i = 0; i < COUNT * 2; i += 2) {
-            float x = src[i + 0];
-            float y = src[i + 1];
-            float dx = cx - x;
-            float dy = cy - y;
-            float dd = dx * dx + dy * dy;
-            float d = (float) Math.sqrt(dd);
-            float pull = (K / (dd + 0.000001f));
-
-            pull /= (d + 0.000001f);
-            //   android.util.Log.d("skia", "index " + i + " dist=" + d + " pull=" + pull);
-
-            if (pull >= 1) {
-                dst[i + 0] = cx;
-                dst[i + 1] = cy;
-            } else {
-                dst[i + 0] = x + dx * pull;
-                dst[i + 1] = y + dy * pull;
+                if (pull >= 1) {
+                    dst[i + 0] = cx;
+                    dst[i + 1] = cy;
+                } else {
+                    dst[i + 0] = x + dx * pull;
+                    dst[i + 1] = y + dy * pull;
+                }
             }
+
+        }
+
+        private int mLastWarpX = -9999; // don't match a touch coordinate
+        private int mLastWarpY;
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            float[] pt = { event.getX(), event.getY() };
+            mInverse.mapPoints(pt);
+
+            int x = (int) pt[0];
+            int y = (int) pt[1];
+            if (mLastWarpX != x || mLastWarpY != y) {
+                mLastWarpX = x;
+                mLastWarpY = y;
+                warp(pt[0], pt[1]);
+                invalidate();
+            }
+            return true;
         }
     }
-
-    private void setXY(float[] array, int index, float x, float y) {
-        array[index * 2 + 0] = x;
-        array[index * 2 + 1] = y;
-    }
-
-    private int mLastWarpX = -9999; // don't match a touch coordinate
-    private int mLastWarpY;
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        float[] pt = {event.getX(), event.getY()};
-        mInverse.mapPoints(pt);
-
-        int x = (int) pt[0];
-        int y = (int) pt[1];
-        if (mLastWarpX != x || mLastWarpY != y) {
-            mLastWarpX = x;
-            mLastWarpY = y;
-            warp(pt[0], pt[1]);
-            imgView.invalidate();
-        }
-        return false;
-    }
-
 }
